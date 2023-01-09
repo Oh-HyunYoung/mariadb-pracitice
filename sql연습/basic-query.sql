b9ddea460f3240fde1761e9ffe34ced8c2640936
Oh-HyunYoung <gusdud2709@gmail.com>
2023-01-09 12:15:22 +0900

    select #1

--- sql연습/basic-query.sql
+++ sql연습/basic-query.sql
@@ -6,7 +6,7 @@ select sin(pi()/4), 1 + 2 * 3 - 4 / 5 from dual;
 -- 대소문자 구분 안한다.
 select VERSION(), current_DAte, now() from dual;
 
--- table 생성: DML
+-- table 생성: DDL
 create table pet (
 	name varchar(100),
 	owner varchar(20),
@@ -35,8 +35,70 @@ select * from pet;
 -- update: DML(U)
 update pet set name = '성타니' where name = '성탄이';
 
+update pet
+	set death = null
+	where name != 'Bowser';
+
 -- delete: DML(D)
 delete from pet where name = '성타니';
 
 -- load data
 load data local infile 'd:\pet.txt' into table pet;
+
+-- select 연습
+select name, species
+from pet
+where name = 'Bowser';
+
+select name, species
+from pet
+where birth >= '1998-01-01';
+
+select name, species, gender
+from pet
+where species = 'dog'
+and gender = 'f';
+
+select name, species
+  from pet
+where species = 'snake'
+	or species = 'bird';
+
+  select name, birth
+	from pet
+order by birth asc;
+
+  select name, birth
+	from pet
+order by birth desc;
+
+select name, birth, death
+  from pet
+ where death is not null;
+
+select name
+	from pet
+  where name like 'b%';
+
+select name
+	from pet
+  where name like '%fy';
+
+select name
+	from pet
+  where name like '%w%';
+
+select name
+	from pet
+  where name like '_____';
+
+select name
+	from pet
+  where name like 'b____';
+
+select count(*)
+   from pet
+	where death is not null;
+    
+
+  
