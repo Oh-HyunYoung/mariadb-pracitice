d1cf2f0ab19be35870a9175b4f067c7a334c0234
Oh-HyunYoung <gusdud2709@gmail.com>
2023-01-12 21:32:36 +0900

    sql #upload

--- jdbc-practice/sql/emaillist.sql
+++ jdbc-practice/sql/emaillist.sql
@@ -5,6 +5,14 @@ insert into emaillist values(null, '둘','리','dooly@gmail.com');
 
 -- delete
 delete from emaillist where email = 'dooly@gmail.com';
-
+delete from author;
+delete from book;
 -- list
 select no, first_name, last_name, email from emaillist order by no desc;
+select no, title,author_no,rent from book order by no desc;
+select * from author order by no desc;
+select * from book order by no desc;
+
+select * from author order by no desc;
+insert into author values(null, '둘리');
+insert into book(no,title,author_no) values(null,?,?);
